0489aa18b547173f00c7b0f58a60f94514bda075
Viktorlovas1 <130307886+Viktorlovas1@users.noreply.github.com>
2026-09-09 21:22:38 +0200

    Show only Early Access on public homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { AccessGate } from "@/components/access-gate";
-import { ACCESS_COOKIE_NAME, isAccessCookieValid } from "@/lib/access";
+import { EarlyAccessGate } from "@/components/access-gate";
 
-export default async function Home() {
-  const cookieStore = await cookies();
-  const accessCookie = cookieStore.get(ACCESS_COOKIE_NAME)?.value;
-  if (await isAccessCookieValid(accessCookie)) redirect("/shop");
-  return <AccessGate />;
+export default function Home() {
+  return <EarlyAccessGate />;
 }
