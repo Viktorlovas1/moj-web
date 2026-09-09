ab4489f5a6866fde043e6700bbceb50786ba4919
Viktorlovas1 <130307886+Viktorlovas1@users.noreply.github.com>
2026-09-09 23:14:51 +0200

    Use German Shopify privacy policy

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 
-const SHOPIFY_PRIVACY_POLICY_URL = "https://checkout.shopify.com/101428101450/policies/59035517258.html?locale=en";
+const SHOPIFY_PRIVACY_POLICY_URL =
+  "https://checkout.shopify.com/101428101450/policies/59035517258.html?locale=de";
 
 export default function DatenschutzPage() {
   redirect(SHOPIFY_PRIVACY_POLICY_URL);
