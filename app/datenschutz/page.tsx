ef7d2b4c40a703d5b397b9cf0dd2699c99fb0033
Viktorlovas1 <130307886+Viktorlovas1@users.noreply.github.com>
2026-09-09 22:58:12 +0200

    Use Shopify policy URL for Datenschutz

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-const SHOPIFY_PRIVACY_POLICY_URL = "https://zx0n6q-qg.myshopify.com/policies/privacy-policy";
+const SHOPIFY_PRIVACY_POLICY_URL = "https://checkout.shopify.com/101428101450/policies/59035517258.html?locale=en";
 
 export default function DatenschutzPage() {
   redirect(SHOPIFY_PRIVACY_POLICY_URL);
